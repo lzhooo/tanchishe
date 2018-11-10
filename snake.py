95dde32f9a9f9942f0ee265237ee99d0c00e167f
3116005143 <elricleung@126.com>
2018-11-10 23:19:52 +0800

    添加单人模式

--- snake.py
+++ snake.py
@@ -5,33 +5,33 @@ import time
 pygame.init()
 
 
-class vr:
-    gw = 25
-    gh = 25
-    pxl = 25
-    wl = True
+class block:
+    blockwidth = 25
+    blockheight = 25
+    blocksize = 25
+    wall = True
     points = 0
     coloroffset = 5
     done = False
 
 
-vr.sw = vr.gw * vr.pxl
-vr.sh = vr.gh * vr.pxl
+block.screenwidth = block.blockwidth * block.blocksize
+block.screenheight = block.blockheight * block.blocksize
 clock = pygame.time.Clock()
-screen = pygame.display.set_mode((vr.sw, vr.sh))
+screen = pygame.display.set_mode((block.screenwidth, block.screenheight))
 c = 0
 
 
-class apple:
-    x = random.randint(1, vr.gw - 2)
-    y = random.randint(1, vr.gh - 2)
+class bean:
+    x = random.randint(1, block.blockwidth - 2)
+    y = random.randint(1, block.blockheight - 2)
     lvl = 0
 
 
 class snake:
     leng = 4
-    x = random.randint(1, vr.gw - 2)
-    y = random.randint(1, vr.gh - 2)
+    x = random.randint(1, block.blockwidth - 2)
+    y = random.randint(1, block.blockheight - 2)
     dire = 5
     speed = 10
     tailx = []
@@ -44,8 +44,8 @@ class snake:
 
 class snake2:
     leng = 4
-    x = random.randint(1, vr.gw - 2)
-    y = random.randint(1, vr.gh - 2)
+    x = random.randint(1, block.blockwidth - 2)
+    y = random.randint(1, block.blockheight - 2)
     dire = 5
     speed = 10
     tailx = []
@@ -56,11 +56,11 @@ class snake2:
     score = 0
 
 
-class gamef:
+class game:
     def death():
         snake.leng = 4
-        snake.x = random.randint(1, vr.gw - 2)
-        snake.y = random.randint(1, vr.gh - 2)
+        snake.x = random.randint(1, block.blockwidth - 2)
+        snake.y = random.randint(1, block.blockheight - 2)
         snake.dire = 5
         snake.speed = 10
         snake.tailx = []
@@ -75,8 +75,8 @@ class gamef:
 
     def death2():
         snake2.leng = 4
-        snake2.x = random.randint(1, vr.gw - 2)
-        snake2.y = random.randint(1, vr.gh - 2)
+        snake2.x = random.randint(1, block.blockwidth - 2)
+        snake2.y = random.randint(1, block.blockheight - 2)
         snake2.dire = 5
         snake2.speed = 10
         snake2.tailx = []
@@ -89,35 +89,35 @@ class gamef:
         else:
             snake2.score -= 5
 
-    def grid():
-        for x in range(int(vr.sw / vr.pxl)):
-            for y in range(int(vr.sh / vr.pxl)):
-                pygame.draw.rect(screen, (32, 32, 32), pygame.Rect(x * vr.pxl, y * vr.pxl, vr.pxl, vr.pxl))
+    def square():
+        for x in range(int(block.screenwidth / block.blocksize)):
+            for y in range(int(block.screenheight / block.blocksize)):
+                pygame.draw.rect(screen, (32, 32, 32), pygame.Rect(x * block.blocksize, y * block.blocksize, block.blocksize, block.blocksize))
 
     def draw():
         # Framerate of 10
-        pygame.draw.rect(screen, (8, 8, 8), pygame.Rect(0, 0, vr.pxl, vr.sh))
-        pygame.draw.rect(screen, (8, 8, 8), pygame.Rect(vr.sw - vr.pxl, 0, vr.pxl, vr.sh))
-        pygame.draw.rect(screen, (8, 8, 8), pygame.Rect(0, 0, vr.sw, vr.pxl))
-        pygame.draw.rect(screen, (8, 8, 8), pygame.Rect(0, vr.sh - vr.pxl, vr.sw, vr.pxl))
+        pygame.draw.rect(screen, (8, 8, 8), pygame.Rect(0, 0, block.blocksize, block.screenheight))
+        pygame.draw.rect(screen, (8, 8, 8), pygame.Rect(block.screenwidth - block.blocksize, 0, block.blocksize, block.screenheight))
+        pygame.draw.rect(screen, (8, 8, 8), pygame.Rect(0, 0, block.screenwidth, block.blocksize))
+        pygame.draw.rect(screen, (8, 8, 8), pygame.Rect(0, block.screenheight - block.blocksize, block.screenwidth, block.blocksize))
         color = (0, 255, 0)
-        pygame.draw.rect(screen, color, pygame.Rect(vr.pxl * apple.x + 2, vr.pxl * apple.y + 2, vr.pxl - 4, vr.pxl - 4))
-        pygame.draw.rect(screen, color, pygame.Rect(vr.pxl * apple.x + 2, vr.pxl * apple.y + 2, vr.pxl - 4, vr.pxl - 4))
+        pygame.draw.rect(screen, color, pygame.Rect(block.blocksize * bean.x + 2, block.blocksize * bean.y + 2, block.blocksize - 4, block.blocksize - 4))
+        pygame.draw.rect(screen, color, pygame.Rect(block.blocksize * bean.x + 2, block.blocksize * bean.y + 2, block.blocksize - 4, block.blocksize - 4))
         for i in range(len(snake.tailx)):
             color = snake.color
             pygame.draw.rect(screen, color,
-                             pygame.Rect(vr.pxl * snake.tailx[i], vr.pxl * snake.taily[i], vr.pxl, vr.pxl))
+                             pygame.Rect(block.blocksize * snake.tailx[i], block.blocksize * snake.taily[i], block.blocksize, block.blocksize))
         color = snake.head_color
-        pygame.draw.rect(screen, color, pygame.Rect(vr.pxl * snake.x, vr.pxl * snake.y, vr.pxl, vr.pxl))
+        pygame.draw.rect(screen, color, pygame.Rect(block.blocksize * snake.x, block.blocksize * snake.y, block.blocksize, block.blocksize))
 
         for i in range(len(snake2.tailx)):
             color = snake2.color
             pygame.draw.rect(screen, color,
-                             pygame.Rect(vr.pxl * snake2.tailx[i], vr.pxl * snake2.taily[i], vr.pxl, vr.pxl))
+                             pygame.Rect(block.blocksize * snake2.tailx[i], block.blocksize * snake2.taily[i], block.blocksize, block.blocksize))
         color = snake2.head_color
-        pygame.draw.rect(screen, color, pygame.Rect(vr.pxl * snake2.x, vr.pxl * snake2.y, vr.pxl, vr.pxl))
+        pygame.draw.rect(screen, color, pygame.Rect(block.blocksize * snake2.x, block.blocksize * snake2.y, block.blocksize, block.blocksize))
 
-    def keyd():
+    def keyboard():
         pressed = pygame.key.get_pressed()
         if pressed[pygame.K_UP]:
             if (snake.dire != 2):
@@ -146,144 +146,144 @@ class gamef:
                 snake2.dire = 3
 
     def ref():
-        gamef.tails()
+        game.tails()
         if (snake.dire == 0):
-            gamef.move(0, -1)
+            game.move(0, -1)
             # snake.y -=1
         elif (snake.dire == 1):
-            gamef.move(1, 0)
+            game.move(1, 0)
             # snake.x+=1
         elif (snake.dire == 2):
             # snake.y+=1
-            gamef.move(0, 1)
+            game.move(0, 1)
         elif (snake.dire == 3):
-            gamef.move(-1, 0)
+            game.move(-1, 0)
             # snake.x-=1
 
     def ref2():
-        gamef.tails2()
+        game.tails2()
         if (snake2.dire == 0):
-            gamef.move2(0, -1)
+            game.move2(0, -1)
             # snake.y -=1
         elif (snake2.dire == 1):
-            gamef.move2(1, 0)
+            game.move2(1, 0)
             # snake.x+=1
         elif (snake2.dire == 2):
             # snake.y+=1
-            gamef.move2(0, 1)
+            game.move2(0, 1)
         elif (snake2.dire == 3):
-            gamef.move2(-1, 0)
+            game.move2(-1, 0)
             # snake.x-=1
 
     def move(x, y):
         # x check
-        if (snake.x + x) >= vr.gw - 1:
-            if (vr.wl):
+        if (snake.x + x) >= block.blockwidth - 1:
+            if (block.wall):
                 snake.x = 1
             else:
-                gamef.death()
+                game.death()
             # print ("out of bounds")
 
         elif (snake.x + x) <= 0:
             # print ("out of bounds")
-            if (vr.wl):
-                snake.x = vr.gw - 2
+            if (block.wall):
+                snake.x = block.blockwidth - 2
             else:
-                gamef.death()
+                game.death()
 
         else:
             snake.x += x
         # y check
-        if (snake.y + y) >= vr.gh - 1:
+        if (snake.y + y) >= block.blockheight - 1:
             # print ("out of bounds")
-            if (vr.wl):
+            if (block.wall):
                 snake.y = 1
             else:
-                gamef.death()
+                game.death()
         elif (snake.y + y) <= 0:
             # print ("out of bounds")
-            if (vr.wl):
-                snake.y = vr.gh - 2
+            if (block.wall):
+                snake.y = block.blockheight - 2
             else:
-                gamef.death()
+                game.death()
 
         else:
             snake.y += y
-        # apple
-        if (snake.x == apple.x) and (snake.y == apple.y):
+        # bean
+        if (snake.x == bean.x) and (snake.y == bean.y):
             snake.leng += 1
-            apple.lvl += 1
+            bean.lvl += 1
             snake.score += 10
-            apple.x = random.randint(1, vr.gw - 2)
-            apple.y = random.randint(1, vr.gh - 2)
-            # print("Apple pos:\nX - "+str(apple.x)+"\nY - "+str(apple.y))
+            bean.x = random.randint(1, block.blockwidth - 2)
+            bean.y = random.randint(1, block.blockheight - 2)
+            # print("bean pos:\nX - "+str(bean.x)+"\nY - "+str(bean.y))
         cdeaths = snake.deaths;
         for i in range(len(snake.tailx)):
             if (snake.deaths == cdeaths):
                 if (snake.x == snake.tailx[i]):
                     if (snake.y == snake.taily[i]):
                         # print("game over")
-                        gamef.death()
-        gamef.crash(snake.x, snake.y, snake2.tailx, snake2.taily, 1)
+                        game.death()
+        game.crash(snake.x, snake.y, snake2.tailx, snake2.taily, 1)
 
     def move2(x, y):
         # x check
-        if (snake2.x + x) >= vr.gw - 1:
-            if (vr.wl):
+        if (snake2.x + x) >= block.blockwidth - 1:
+            if (block.wall):
                 snake2.x = 1
             else:
-                gamef.death2()
+                game.death2()
             # print ("out of bounds")
 
         elif (snake2.x + x) <= 0:
             # print ("out of bounds")
-            if (vr.wl):
-                snake2.x = vr.gw - 2
+            if (block.wall):
+                snake2.x = block.blockwidth - 2
             else:
-                gamef.death2()
+                game.death2()
 
         else:
             snake2.x += x
         # y check
-        if (snake2.y + y) >= vr.gh - 1:
+        if (snake2.y + y) >= block.blockheight - 1:
             # print ("out of bounds")
-            if (vr.wl):
+            if (block.wall):
                 snake2.y = 1
             else:
-                gamef.death2()
+                game.death2()
         elif (snake2.y + y) <= 0:
             # print ("out of bounds")
-            if (vr.wl):
-                snake2.y = vr.gh - 2
+            if (block.wall):
+                snake2.y = block.blockheight - 2
             else:
-                gamef.death2()
+                game.death2()
 
         else:
             snake2.y += y
-        # apple
-        if (snake2.x == apple.x) and (snake2.y == apple.y):
+        # bean
+        if (snake2.x == bean.x) and (snake2.y == bean.y):
             snake2.leng += 1
-            apple.lvl += 1
-            apple.x = random.randint(1, vr.gw - 2)
-            apple.y = random.randint(1, vr.gh - 2)
+            bean.lvl += 1
+            bean.x = random.randint(1, block.blockwidth - 2)
+            bean.y = random.randint(1, block.blockheight - 2)
             snake2.score += 10
-            # print("Apple pos:\nX - "+str(apple.x)+"\nY - "+str(apple.y))
+            # print("bean pos:\nX - "+str(bean.x)+"\nY - "+str(bean.y))
         cdeaths = snake2.deaths
         for i in range(len(snake2.tailx)):
             if (snake2.deaths == cdeaths):
                 if (snake2.x == snake2.tailx[i]):
                     if (snake2.y == snake2.taily[i]):
                         # print("game over")
-                        gamef.death2()
-        gamef.crash(snake2.x, snake2.y, snake.tailx, snake.taily, 2)
+                        game.death2()
+        game.crash(snake2.x, snake2.y, snake.tailx, snake.taily, 2)
 
     def crash(x1, y1, x2, y2, key):
         for i in range(len(x2)):
             if x1 == x2[i] and y1 == y2[i]:
                 if key == 1:
-                    gamef.death()
+                    game.death()
                 if key == 2:
-                    gamef.death2()
+                    game.death2()
 
     def tails():
         snake.tailx.append(snake.x)
@@ -300,20 +300,20 @@ class gamef:
             snake2.taily.pop(0)
 
 
-while not vr.done:
+while not block.done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            vr.done = True
+            block.done = True
             exit()
             quit()
 
-    gamef.grid()
-    gamef.keyd()
+    game.square()
+    game.keyboard()
     c += 1
-    gamef.draw()
+    game.draw()
     if (c >= (100 / snake.speed)):
-        gamef.ref()
-        gamef.ref2()
+        game.ref()
+        game.ref2()
         c = 0
     pygame.display.set_caption(" P1 Score: " + str(snake.score) + " P2 Score: " + str(snake2.score))
     pygame.display.flip()
